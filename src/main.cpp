5a19ca6af7f6801f9f7ac12ee8ed47b94a949aa1
JulV <julvdev@gmail.com>
2016-05-04 12:59:46 +0000

    new hello world (ros one)

--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,151 @@
-/******************************************************************************
-* Author:
-* Julien Van Loo
-******************************************************************************/
+//
+// Simple demo program that calls the youBot ROS wrapper
+//
 
-#include <vector>
-#include "youbot_driver/youbot/YouBotBase.hpp"
-#include "youbot_driver/youbot/YouBotManipulator.hpp"
+#include "ros/ros.h"
+#include "boost/units/systems/si.hpp"
+#include "boost/units/io.hpp"
+#include "brics_actuator/JointPositions.h"
+#include "geometry_msgs/Twist.h"
 
 using namespace std;
-using namespace youbot;
 
-YouBotBase* createBase() {
-    /* create a youbot base, return the base or NULL if could not create it */
-    YouBotBase* base;
-    try {
-        base = new YouBotBase("youbot-base", YOUBOT_CONFIGURATIONS_DIR);
-        base->doJointCommutation();
-    } catch (std::exception& e) {
-        LOG(warning) << e.what();
-        base = NULL;
+ros::Publisher platformPublisher;
+ros::Publisher armPublisher;
+ros::Publisher gripperPublisher;
+
+// create a brics actuator message with the given joint position values
+brics_actuator::JointPositions createArmPositionCommand(vector<double>& newPositions) {
+    int numberOfJoints = 5;
+    brics_actuator::JointPositions msg;
+
+    if (newPositions.size() < numberOfJoints)
+        return msg; // return empty message if not enough values provided
+
+    for (int i = 0; i < numberOfJoints; i++) {
+        // Set all values for one joint, i.e. time, name, value and unit
+        brics_actuator::JointValue joint;
+        joint.timeStamp = ros::Time::now();
+        joint.value = newPositions[i];
+        joint.unit = boost::units::to_string(boost::units::si::radian);
+
+        // create joint names: "arm_joint_1" to "arm_joint_5" (for 5 DoF)
+        stringstream jointName;
+        jointName << "arm_joint_" << (i + 1);
+        joint.joint_uri = jointName.str();
+
+        // add joint to message
+        msg.positions.push_back(joint);
     }
-    return base;
+
+    return msg;
 }
 
-YouBotManipulator* createArm() {
-    /* create a youbot arm, return the arm or NULL if could not create it */
-    YouBotManipulator* arm;
-    try {
-        arm = new YouBotManipulator("youbot-manipulator", YOUBOT_CONFIGURATIONS_DIR);
-        arm->doJointCommutation();
-        arm->calibrateManipulator();
-    } catch (std::exception& e) {
-        LOG(warning) << e.what();
-        arm = NULL;
-    }
-    return arm;
+// create a brics actuator message for the gripper using the same position for both fingers
+brics_actuator::JointPositions createGripperPositionCommand(double newPosition) {
+    brics_actuator::JointPositions msg;
+
+    brics_actuator::JointValue joint;
+    joint.timeStamp = ros::Time::now();
+    joint.unit = boost::units::to_string(boost::units::si::meter); // = "m"
+    joint.value = newPosition;
+    joint.joint_uri = "gripper_finger_joint_l";
+    msg.positions.push_back(joint);
+    joint.joint_uri = "gripper_finger_joint_r";
+    msg.positions.push_back(joint);
+
+    return msg;
 }
 
-void setBasePosition(YouBotBase* base, vector<int> position, int orientation, int velocity) {
-    base->setBaseVelocity(longitudinalVelocity, transversalVelocity, orientation*radians_per_second);
+
+// move platform a little bit back- and forward and to the left and right
+void movePlatform() {
+    geometry_msgs::Twist twist;
+
+    // forward
+    twist.linear.x = 0.05;  // with 0.05 m per sec
+    platformPublisher.publish(twist);
+    ros::Duration(2).sleep();
+
+    // backward
+    twist.linear.x = -0.05;
+    platformPublisher.publish(twist);
+    ros::Duration(2).sleep();
+
+    // to the left
+    twist.linear.x = 0;
+    twist.linear.y = 0.05;
+    platformPublisher.publish(twist);
+    ros::Duration(2).sleep();
+
+    // to the right
+    twist.linear.y = -0.05;
+    platformPublisher.publish(twist);
+    ros::Duration(2).sleep();
+
+    // stop
+    twist.linear.y = 0;
+    platformPublisher.publish(twist);
 }
 
-int main() {
-	/* define velocities */
-	double translationalVelocity = 0.05; //meter_per_second
-	double rotationalVelocity = 0.2; //radian_per_second
+// move arm once up and down
+void moveArm() {
+    brics_actuator::JointPositions msg;
+    vector<double> jointvalues(5);
 
-	/* create handles for youBot base and manipulator (if available) */
-    YouBotBase* base = createBase();
-    YouBotManipulator* arm = createArm();
+    // move arm straight up. values were determined empirically
+    jointvalues[0] = 2.95;
+    jointvalues[1] = 1.05;
+    jointvalues[2] = -2.44;
+    jointvalues[3] = 1.73;
+    jointvalues[4] = 2.95;
+    msg = createArmPositionCommand(jointvalues);
+    armPublisher.publish(msg);
 
-	/*
-	* Variable for the base.
-	* Here "boost units" is used to set values in OODL, that means you have to set a value and a unit.
-	*/
-	quantity<si::velocity> longitudinalVelocity = 0 * meter_per_second;
-	quantity<si::velocity> transversalVelocity = 0 * meter_per_second;
-	quantity<si::angular_velocity> angularVelocity = 0 * radian_per_second;
+    ros::Duration(5).sleep();
 
-	/* Variable for the arm. */
-	JointAngleSetpoint desiredJointAngle;
+    // move arm back close to calibration position
+    jointvalues[0] = 0.11;
+    jointvalues[1] = 0.11;
+    jointvalues[2] = -0.11;
+    jointvalues[3] = 0.11;
+    jointvalues[4] = 0.111;
+    msg = createArmPositionCommand(jointvalues);
+    armPublisher.publish(msg);
 
-	try {
-		/*
-		 * Simple sequence of commands to the youBot:
-		 */
-
-        if (base) {
-
-			/* forward */
-			longitudinalVelocity = translationalVelocity * meter_per_second;
-			transversalVelocity = 0 * meter_per_second;
-            base->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
-			LOG(info) << "drive forward";
-			SLEEP_MILLISEC(2000);
-
-			/* backwards */
-			longitudinalVelocity = -translationalVelocity * meter_per_second;
-			transversalVelocity = 0 * meter_per_second;
-            base->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
-			LOG(info) << "drive backwards";
-			SLEEP_MILLISEC(2000);
-
-			/* left */
-			longitudinalVelocity = 0 * meter_per_second;
-			transversalVelocity = translationalVelocity * meter_per_second;
-			angularVelocity = 0 * radian_per_second;
-            base->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
-			LOG(info) << "drive left";
-			SLEEP_MILLISEC(2000);
-
-			/* right */
-			longitudinalVelocity = 0 * meter_per_second;
-			transversalVelocity = -translationalVelocity * meter_per_second;
-			angularVelocity = 0 * radian_per_second;
-            base->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
-			LOG(info) << "drive right";
-			SLEEP_MILLISEC(2000);
-
-			/* stop base */
-			longitudinalVelocity = 0 * meter_per_second;
-			transversalVelocity = 0 * meter_per_second;
-			angularVelocity = 0 * radian_per_second;
-            base->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
-			LOG(info) << "stop base";
-		}
-
-        if (arm) {
-
-			/* unfold arm 
-			 * all of the following constants are empirically determined to move the arm into the desired position 
-			 */
-			desiredJointAngle.angle = 2.56244 * radian;
-            arm->getArmJoint(1).setData(desiredJointAngle);
-
-			desiredJointAngle.angle = 1.04883 * radian;
-            arm->getArmJoint(2).setData(desiredJointAngle);
-
-			desiredJointAngle.angle = -2.43523 * radian;
-            arm->getArmJoint(3).setData(desiredJointAngle);
-
-			desiredJointAngle.angle = 1.73184 * radian;
-            arm->getArmJoint(4).setData(desiredJointAngle);
-			LOG(info) << "unfold arm";
-			SLEEP_MILLISEC(4000);
-
-			/* fold arm (approx. home position) using empirically determined values for the positions */
-			desiredJointAngle.angle = 0.11 * radian;
-            arm->getArmJoint(1).setData(desiredJointAngle);
-
-			desiredJointAngle.angle = 0.11 * radian;
-            arm->getArmJoint(2).setData(desiredJointAngle);
-
-			desiredJointAngle.angle = -0.11 * radian;
-            arm->getArmJoint(3).setData(desiredJointAngle);
-			desiredJointAngle.angle = 0.11 * radian;
-            arm->getArmJoint(4).setData(desiredJointAngle);
-			LOG(info) << "fold arm";
-			SLEEP_MILLISEC(4000);
-		}
-
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-		std::cout << "unhandled exception" << std::endl;
-	}
-
-	/* clean up */
-    if (base) {
-        delete base;
-        //myYouBotBase = 0;
-	}
-    if (arm) {
-        delete arm;
-        //myYouBotManipulator = 0;
-	}
-
-	LOG(info) << "Done.";
-
-	return 0;
+    ros::Duration(2).sleep();
 }
 
+// open and close gripper
+void moveGripper() {
+    brics_actuator::JointPositions msg;
+
+    // open gripper
+    msg = createGripperPositionCommand(0.011);
+    gripperPublisher.publish(msg);
+
+    ros::Duration(3).sleep();
+
+    // close gripper
+    msg = createGripperPositionCommand(0);
+    gripperPublisher.publish(msg);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "youbot_ros_hello_world");
+    ros::NodeHandle n;
+
+    platformPublisher = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    armPublisher = n.advertise<brics_actuator::JointPositions>("arm_1/arm_controller/position_command", 1);
+    gripperPublisher = n.advertise<brics_actuator::JointPositions>("arm_1/gripper_controller/position_command", 1);
+    sleep(1);
+
+    movePlatform();
+    moveArm();
+    moveGripper();
+
+    sleep(1);
+    ros::shutdown();
+
+    return 0;
+}
